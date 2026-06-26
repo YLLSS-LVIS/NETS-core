4a22c4d3cf2f0ca11613133705eb905e5388d36e
YLLSS-LVIS <teslatikey2022@gmail.com>
2026-06-27 06:25:20 +0800

    Add order cancellation and related functions to clob.py

--- src/clob.py
+++ src/clob.py
@@ -1,9 +1,9 @@
 # TODO: add lifting TOB function
-#
+
 from exchange_data import exchange_data as exchg_data
 from market import Market
+from positions import positions as positions
 from sortedcontainers import SortedDict as sd
-from user_positions import user_positions as positions
 
 
 class clob:
@@ -34,6 +34,7 @@ class clob:
         self._allocOrder = exchange_data.get_order_slot
         self._deallocOrder = exchange_data.release_order_slot
         self.orderID = exchange_data.orderID
+        self.orderMPID = exchange_data.orderMPID
         self.orderMarket = exchange_data.orderMarket
         self.orderOutcome = exchange_data.orderOutcome
         self.orderPrice = exchange_data.orderPrice
@@ -69,6 +70,24 @@ class clob:
             self.clobList[mkt_tail].headClobs[side] = mkt_head
         else:
             self.marketTailClobs[side] = mkt_head
+
+    def deduct_price_lvl(self, side, price, sum_orders, sum_qty):
+        side_book = self.books[side]
+        book_price = price * [-1, 1][side]
+        price_lvl = side_book[book_price]
+        price_lvl[4] -= sum_orders
+        price_lvl[5] -= sum_qty
+
+        if price_lvl[4] > 0:
+            return False
+
+        head_price, tail_price = price_lvl[0:2]
+        if head_price is not None:
+            side_book[head_price][1] = tail_price
+        if tail_price is not None:
+            side_book[tail_price][0] = head_price
+        del side_book[book_price]
+        return True
 
     def post_order(self, mpid, price, side, qty):
         # Verify that the order can be placed by the account, including the consideration of account order limit, global memory limit
@@ -138,3 +157,20 @@ class clob:
                 tail_price_head_order = side_book[tail_price]
                 self.orderClobTail[new_order_idx] = tail_price_head_order
                 self.orderClobHead[tail_price_head_order] = new_order_idx
+
+    def cancel_order(self, order_idx):
+        order_mpid = self.orderMPID[order_idx]
+        order_price = self.orderPrice[order_idx]
+        order_side = self.orderSide[order_idx]
+        order_qty = self.orderQty[order_idx]
+        self.userPositions.cancel_order(order_mpid, order_price, order_side, order_qty)
+
+        order_head = self.orderClobHead[order_idx]
+        order_tail = self.orderClobTail[order_idx]
+        if order_head != -1:
+            self.orderClobTail[order_head] = order_tail
+        if order_tail != -1:
+            self.orderClobHead[order_tail] = order_head
+
+        self.deduct_price_lvl(order_side, order_price, 1, order_qty)
+        self._deallocOrder(order_mpid, order_idx)
